e6d6677a90b004f213960755a664db979050e640
Hyewon Cho and Mel Yasuda <hyewon@>
2018-08-06 15:59:37 -0700

    Add javascript functions

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,28 @@ console.log(sentence);
 var firstStep = function(sentence) {
   var firstLetter = sentence.charAt(0).toUpperCase();
   var lastLetter = sentence.charAt(sentence.length-1).toUpperCase();
-  var middle = sentence.substr(1, sentence.length-2);
-  return firstLetter + middle + lastLetter;
+  return firstLetter + lastLetter;
 }
-console.log(firstStep(sentence));
+
+var secondStep = function(letters){
+  var firstLetter = letters.charAt(0);
+  var lastLetter = letters.charAt(1);
+  return lastLetter + firstLetter;
+}
+
+var thirdStep = function(sentence) {
+  return sentence+secondStep(firstStep(sentence));
+}
+
+var fourthStep = function(sentence){
+  var length = sentence.length;
+  var halfLength = length/2;
+  var divided = halfLength - halfLength % 1;
+  var halfChar = sentence.charAt(divided);
+  var result = halfChar + thirdStep(sentence);
+
+  var array = result.split("").reverse();
+  return array.join("");
+}
+
+console.log(fourthStep(sentence));
